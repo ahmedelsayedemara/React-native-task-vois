e2099309f7421eca1dc4052e2aae6924c95ddd44
ahmed emara <36634529+ahmedelsayedemara@users.noreply.github.com>
2022-01-14 22:04:44 +0200

    Upload image

--- src/api/ApiService/index.js
+++ src/api/ApiService/index.js
@@ -12,16 +12,3 @@ export default function ApiService({ method, url, config = {} }) {
 
   });
 }
-
-axios.interceptors.response.use(
-  (response) => response,
-  (error) => {
-
-
-    if (error.response.status === 401) {
-      console.log('Error :>> ');
-    }
-
-    return Promise.reject(error);
-  }
-);
--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,36 +1,61 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Books from "../views/Books/list"
 import AddBook from "../views/Books/AddBook"
 const Stack = createNativeStackNavigator();
-import { Button } from 'react-native';
+import { Image, TouchableOpacity, StyleSheet } from 'react-native';
 
 const RouteNavigator = () => {
+  const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: '#fff'
+    },
+  };
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
+    <NavigationContainer theme={MyTheme}>
+      <Stack.Navigator >
 
         <Stack.Screen name="Books" component={Books}
 
           options={({ navigation }) => ({
+
+            animation: 'slide_from_right',
+            headerTitleAlign: "center",
+            title: "Home",
             headerRight: () => (
-              <Button
-                onPress={() => navigation.navigate('AddBook')}
-                title="Add new book"
-              />
+              <TouchableOpacity onPress={() => navigation.navigate('AddBook')}>
+                <Image
+                  style={styles.headerIcon}
+                  source={require('../../assets/add.svg')}
+
+                />
+              </TouchableOpacity>
             ),
           })}
         />
         <Stack.Screen name="AddBook" component={AddBook}
           options={() => ({
-            title: 'Add new book',
+            title: 'Home',
           })}
         />
       </Stack.Navigator>
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  headerIcon: {
+    paddingHorizontal: 10,
+    width: 28,
+    height: 28
+  },
+  contentStyle: {
+    backgroundColor: "#000"
+  }
+})
 
 export default RouteNavigator
 
